43601d16ee5d6663752274cba349f1d9eb5e1283
deepfates <58602708+deepfates@users.noreply.github.com>
2026-04-18 23:22:38 -0700

    Update openaiClient.ts

--- server/apis/openaiClient.ts
+++ server/apis/openaiClient.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import { config } from "../config";
 
 export const openai = new OpenAI({
-  baseURL: "http://212.247.220.167/v1",
+  baseURL: "http://laptop.david.mcelroy.online/v1",
   // This endpoint does not require model routing or OpenRouter headers.
   apiKey: config.completionsApiKey,
 });
